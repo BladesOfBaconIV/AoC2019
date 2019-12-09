5cd831c181c6582c5ead607fa8d14b4137a2e021
Lorcan Williamson <lorcan2k11@gmail.com>
2019-12-09 11:10:58 +0000

    Updated day 5 solution to use single IntCodeMachine

--- day5.py
+++ day5.py
@@ -1,14 +1,13 @@
 from virtual_machines import IntCodeMachine
 
-with open('input.txt', 'r') as f:
-    p = list(map(int, f.read().split(',')))
+vm = IntCodeMachine(program='input.txt')
+vm.input_buffer.append(1)
+vm.run()
+part_1_output = set(vm.output_buffer)
 
-vm_part1 = IntCodeMachine(p)
-vm_part1.input_buffer.append(1)
-vm_part1.run()
+vm.respin()
+vm.input_buffer.append(5)
+vm.run()
+assert len(vm.output_buffer) == 1
 
-vm_part2 = IntCodeMachine(p)
-vm_part2.input_buffer.append(5)
-vm_part2.run()
-
-print(f'{set(vm_part1.output_buffer)=}, {vm_part2.output_buffer=}')
+print(f'{part_1_output=}, {vm.output_buffer=}')
